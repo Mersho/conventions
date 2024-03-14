e08eb35084b7658107e4b9bc6aaf1b1886655637
Mehrshad <code.rezaei@gmail.com>
2024-03-14 12:54:33 +0330

    WIP

--- commitlint.sh
+++ commitlint.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 # cd to directory of this script
 cd "$(dirname "$0")"
 npm install --verbose
-npm install --verbose @commitlint/cli
+npm install --verbose @commitlint/cli conventional-changelog-conventionalcommits
 npx commitlint --version
 npx commitlint $@
